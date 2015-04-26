9da2bbdf03c3f359f3b9f997f384998dbcfc35ac
marinab67 <marinab67@gmail.com>
2015-04-26 12:36:37 -0400

    done.

--- fizzbuzzrefactor.js
+++ fizzbuzzrefactor.js
@@ -1,17 +1,20 @@
 function fizzBuzz (integer) { 
-	integer = $("#input").val();
-	for (var i = 1; i <= integer; i++) { 
-		if (i%15==0) {
-			$("#output").append("FizzBuzz<br>");
-		}
-		else if (i%3==0) { 
-			$("#output").append("Fizz<br>"); 
-		}
-		else if (i%5==0) { 
-			$("#output").append("Buzz<br>")
-		}
-		else { 
-			$("#output").append(i + "<br>")
+	if (isNaN($("#input").val()) || ($("#input").val())%1!=0) {alert("enter a whole number!!! thx.");}
+	else {
+	integer = parseInt($("#input").val());
+		for (var i = 1; i <= integer; i++) { 
+			if (i%15==0) {
+				$("#output").append("FizzBuzz<br>");
+			}
+			else if (i%3==0) { 
+				$("#output").append("Fizz<br>"); 
+			}
+			else if (i%5==0) { 
+				$("#output").append("Buzz<br>")
+			}
+			else { 
+				$("#output").append(i + "<br>")
+			}
 		}
 	}
 	$("#input").val("");
